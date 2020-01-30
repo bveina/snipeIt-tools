78ad76908ca5d1b9745270cf4d856d9b5caeeb07
Dr. Benjamin Viall <u_bviall@umassd.edu>
2020-01-30 10:28:34 -0500

    updated CSV parsing from the on campus database export with the correct column names to correctly parse items.

--- AssetControl.py
+++ AssetControl.py
@@ -3,18 +3,18 @@
     the csv files have the following structure
     OrderedDict([('Unit', 'SSSSSS'),
              ('Dept', 'D#########'),
-             ('Custodian', 'Smith,John'),
+             ('Custodian', 'Smith,John'),   #
              ('EMPLID', '12345678'),
-             ('Asset ID', '000000######'),
-             ('Tag Number', '316454'),
-             ('Location', 'ROOM-123'),
+             ('Asset ID', '000000######'),  #
+             ('Tag Number', '316454'),      #
+             ('Location', 'ROOM-123'),      #
              ('Cap Cost', '0.00'),
              ('NonCap Cost', '2250.00'),
              ('Acquired Date', '1/01/2000'),
              ('Descr', 'Fluke Calibrator'),
-             ('Model', '5100B'),
-             ('Serial ID', 'xxxxxxxx'),
-             ('Manufacturer', 'Fluke'),
+             ('Model', '5100B'),            #
+             ('Serial ID', 'xxxxxxxx'),     #
+             ('Manufacturer', 'Fluke'),     #
              ('Asset Subtype', ''),
              ('Custodian Subtype', ''),
              ('Last Inventory', '12/3/2015'),
@@ -22,6 +22,37 @@
              ('PO No.', 'yyyyyyyyyy'),
              ('Project', 'Szzzzzzzzz')])
 """
+
+"""
+*** new export format ***
+Business Unit, UMDAR
+Asset ID,Capital, 000000212848
+Acquisition Date, MM/DD/YYYY
+Index, "PRIMARY" or blank
+Location, SE-214B
+Custodian Dept ID, D269000xxx
+Custodian, "Viall,Ben"
+Inventory Date, MM/DD/YYYY
+Comments, blank
+Amount, $1234.00
+Performed By, APINTFC/Annette/ONLINE
+Description, Desktop dell Optiplex 7060
+Manufacturer, Dell
+Model, Optiplex 760
+Serial ID,
+Tag Number, 
+Subtype, APINTFC ???
+PI ID,
+Taggable,
+Building, "SE"
+Floor, 2
+Profile, H40050 - Lab,Scientific,Research 5 yr
+Custodian ID, 10117417
+Room, 214F
+Description, ECE-Honggang Wang
+Inventory Fiscal Year, 2017
+"""
+
 from apiMenu import getAllAssets
 import os
 import csv
@@ -42,7 +73,7 @@ def loadAssetControl(srcFileName=defaultCSV,filt = None):
 
 def FilterInven(item):
     """ filters an ASSET CONTROL generated csv list for non computer items """
-    if item['Asset Subtype'] in ['LAPTOP','DESKTOP','TABLET','SERVER','PRINTER']: return False
+    if item['Subtype'] in ['LAPTOP','DESKTOP','TABLET','SERVER','PRINTER']: return False
     return True
     
 def FilterComputers(item):
@@ -67,8 +98,8 @@ def FilterInvenPeople(item):
 def cutePrintInvetory(x):
     """ prints a human readable ASSET CONTROL list """
     for ln in x:
-        cost = float(ln['Cap Cost'])+float(ln['NonCap Cost'])
-        print(" {Tag Number:7s} = {Location:10s} - {Descr:35s} - {Model:30s} - {Manufacturer:22s} + {Serial ID:15s} - {Asset Subtype:10s} * {Custodian} - ".format(**ln)+str(cost))
+        cost = float(ln['Amount'])
+        print(" {Tag Number:7s} = {Location:10s} - {Description:35s} - {Model:30s} - {Manufacturer:22s} + {Serial ID:15s} - {Subtype:10s} * {Custodian} - ".format(**ln)+str(cost))
   
 def findAssetControl(tag):
     """ search for an ASSET CONTROL item by tag """
