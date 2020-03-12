6180cf4b296ac7dd50eb7d640c720a789fd04515
Ben Viall <bveina@users.noreply.github.com>
2020-03-12 12:35:07 -0400

    changed interface for inventory Terminal

--- inventoryTerminal.py
+++ inventoryTerminal.py
@@ -1,7 +1,9 @@
 
-from dotenv import load_dotenv
-load_dotenv('.env')
-from apiMenu import *
-from barManual import *
-from AssetControl import *
+
+if __name__ =="__main__":
+ from dotenv import load_dotenv
+ load_dotenv('.env')
+ from apiMenu import *
+ from barManual import *
+ from AssetControl import *
 
