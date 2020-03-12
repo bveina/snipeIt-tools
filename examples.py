72f0e1effc854cff907915cc4cb06d42c497c6b8
Ben Viall <bveina@users.noreply.github.com>
2020-03-12 12:37:13 -0400

    updated exampe scripts for update of Values based on DMI info

--- examples.py
+++ examples.py
@@ -12,6 +12,7 @@ for w in t:
 	
 
 class DmiInventoryObj:
+#Company,Category,Status,Item Name,Asset Tag,Serial Number,Manufacturer,Model,Model Number,CPU,MAC Address,Total Ram
     def __init__(self):
       self.name=""
       self.serial=""
@@ -20,34 +21,41 @@ class DmiInventoryObj:
       self.cpu=""
       self.mac=""
       self.ram=""
+      self.tag=""
     def __init__(self,myDict):
-      self.name=myDict['Name']
-      self.serial=myDict['Serial']
-      self.make=myDict['Make']
+      self.name=myDict['Item Name']
+      self.serial=myDict['Serial Number']
+      self.make=myDict['Manufacturer']
       self.model=myDict['Model']
       self.cpu=myDict['CPU']
-      self.mac=myDict['MAC']
-      self.ram=myDict['RAM']
+      self.mac=myDict['MAC Address']
+      self.ram=myDict['Total Ram']
+      self.tag=myDict['Asset Tag']
 
-
+import traceback
 
 def compareAndUpdate(webItem,getCurrentVal,tempVal,fieldName):
   global doBlind
   try:
     currentVal = getCurrentVal(webItem)
+    if currentVal is not None:
+      currentVal=currentVal.strip()
+    if tempVal is not None:
+      tempVal=tempVal.strip()
     if currentVal != tempVal:
       # if its blank or a case insensitve compare matches dont bother asking
       if doBlind is True:
         res = ""
+      elif currentVal is None:
+        res=""
       elif len(currentVal) == 0:
         res = ""
       elif currentVal.upper() == tempVal.upper():
         res=""
       else:
-        res=input("press enter to update {0} to {1}, N to cancel".format(currentVal,tempVal))
-      
+        res=input("{2}/{3} - press enter to update -{0}- to -{1}-, N to cancel".format(currentVal,tempVal,webItem['serial'],webItem['asset_tag']))
       if res == "":
-       print("setting {0}'s *{1}* field from '{2}' <- '{3}'".format(webItem['serial'],fieldName,currentVal,tempVal))
+       print("setting {0}/{4}'s *{1}* field from '{2}' <- '{3}'".format(webItem['serial'],fieldName,currentVal,tempVal,webItem['asset_tag']))
        update_item(webItem,fieldName,tempVal,Note="Data Pulled Directly from machine")
        item = findThing(webItem['serial'])
        if getCurrentVal(item) == tempVal:
@@ -55,28 +63,35 @@ def compareAndUpdate(webItem,getCurrentVal,tempVal,fieldName):
        else:
           print("failure")
   except Exception as e:
-    print("big problem with setting {0}: {1}".format(fieldName,e))
-    input()
+    print("{2} -  big problem with setting {0}: {1}".format(fieldName,e,webItem['asset_tag']))
+    print("item: {0}".format(item))
+    t=input()
+    if t =="":
+      print(traceback.format_exc())
+      raise e
       
     
-with open('/home/bveina/logs/inventory.csv','rt') as csvFile:
-  reader = csv.DictReader(csvFile)
-  for row in reader:
-    temp = DmiInventoryObj(row)
-    item = findThing(temp.serial)
-    if item is None:
-      print("couledn't find this thing {0} - {1}".format(temp.name,temp.serial))
-      continue
-    if type(item) is list:
-        print ("this item exists more than once!!!")
-        input()
-    
-    
-    compareAndUpdate(item,lambda x:  x['custom_fields']['MAC Address']['value'], 
-        makeProperMAC(temp.mac),"_snipeit_mac_address_1")
-    compareAndUpdate(item,lambda x: x['name'], temp.name,"name")
-    compareAndUpdate(item,lambda x:  x['custom_fields']['CPU']['value'], temp.cpu,"_snipeit_cpu_4")
-    compareAndUpdate(item,lambda x:  x['custom_fields']['RAM']['value'], temp.ram,"_snipeit_ram_5")
+def DoInvenUpdate():    
+  with open('/home/bveina/logs/inventory.csv','rt') as csvFile:
+    reader = csv.DictReader(csvFile)
+    for row in reader:
+      temp = DmiInventoryObj(row)
+      item = findThing(temp.tag)
+      if item is None:
+        item = findThing(temp.serial)
+      if item is None:
+        print("couledn't find this thing {0} - {1}".format(temp.name,temp.serial))
+        continue
+      if type(item) is list:
+          print ("this item exists more than once!!!")
+          input()
+      
+      
+      compareAndUpdate(item,lambda x:  x['custom_fields']['MAC Address']['value'], 
+          makeProperMAC(temp.mac),"_snipeit_mac_address_1")
+      compareAndUpdate(item,lambda x: x['name'], temp.name,"name")
+      compareAndUpdate(item,lambda x:  x['custom_fields']['CPU']['value'], temp.cpu,"_snipeit_cpu_4")
+      compareAndUpdate(item,lambda x:  x['custom_fields']['RAM']['value'], temp.ram,"_snipeit_ram_5")
     
     
     
